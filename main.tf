e69b1a733e1d4df7086001585f06afeb7bf0419f
Rui Lopes <rgl@ruilopes.com>
2023-02-26 14:59:54 +0000

    upgrade to siderolabs/talos 0.1.1

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ terraform {
     # see https://github.com/siderolabs/terraform-provider-talos
     talos = {
       source  = "siderolabs/talos"
-      version = "0.1.0"
+      version = "0.1.1"
     }
   }
 }
